2dc7a32601fd0a2e61bb6e3fe41dc452d61c9c2f
dolgopolov-d <danildol2011@yandex.ru>
2018-02-21 20:43:43 +0300

    Добавление оператора присваивания

--- students/dolgopolov.d/task1/main.cpp
+++ students/dolgopolov.d/task1/main.cpp
@@ -4,17 +4,23 @@ using namespace std;
 	class TTemperature
 	{
 		double tC;
-		double tF;
-		double tK;
-		double tRe;
-		double tR;
 	public:
 		TTemperature(double t) :tC(t)
 		{
-			tF = tC * 9 / 5 + 32;
-			tK = tC + 273;
-			tRe = tC * 0.8;
-			tR = tC * 493.47;
+			
+			
+			
+			
+		}
+		TTemperature &operator=(double _tC)
+		{
+			tC = _tC;
+			return *this;
+		}
+		TTemperature &operator=(const TTemperature &temper)
+		{
+			tC = temper.tC;
+			return *this;
 		}
 		double get_tC()
 		{
@@ -22,27 +28,50 @@ using namespace std;
 		}
 		double get_tF()
 		{
+			double tF;
+			tF = tC * 9 / 5 + 32;
 			return tF;
 		}
 		double get_tK()
 		{
+			double tK;
+			tK = tC + 273;
 			return tK;
 		}
 		double get_tRe()
 		{
+			double tRe;
+			tRe = tC * 0.8;
 			return tRe;
 		}
 		double get_tR()
 		{
+			double tR;
+			tR = tC * 493.47;
 			return tR;
 		}
 	};
 	int main()
 	{
 		double t;
-		cout << "Insert temperature in Celsius\n";
+		cout << "Enter temperature in Celsius\n";
 		cin >> t;
 		TTemperature temp(t);
+		TTemperature temp1(15);
+		cout << "Temperature in Celsius " << temp.get_tC() << endl;
+		cout << "Temperature in Fahrenheit " << temp.get_tF() << endl;
+		cout << "Temperature in Kelvin " << temp.get_tK() << endl;
+		cout << "Temperature in Remyure " << temp.get_tRe() << endl;
+		cout << "Temperature in Rankin " << temp.get_tR() << endl;
+		temp = 5;
+		cout << endl;
+		cout << "Temperature in Celsius " << temp.get_tC() << endl;
+		cout << "Temperature in Fahrenheit " << temp.get_tF() << endl;
+		cout << "Temperature in Kelvin " << temp.get_tK() << endl;
+		cout << "Temperature in Remyure " << temp.get_tRe() << endl;
+		cout << "Temperature in Rankin " << temp.get_tR() << endl;
+		temp = temp1;
+		cout << endl;
 		cout << "Temperature in Celsius " << temp.get_tC() << endl;
 		cout << "Temperature in Fahrenheit " << temp.get_tF() << endl;
 		cout << "Temperature in Kelvin " << temp.get_tK() << endl;
