7dbf4a2c7e0d3354be09d206f1704e0d7b847152
dolgopolov-d <danildol2011@yandex.ru>
2018-04-07 17:15:37 +0400

    Начало работы над task5

---
+++ students/dolgopolov.d/task5/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <locale.h>
+#include <fstream>
+
+using namespace std;
+
+class GorRailWay 
+{
+public:
+	GorRailWay() :trainLastoch(100)
+	{
+
+	}
+	vector <int> trainLastoch;
+};
